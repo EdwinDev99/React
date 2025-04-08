f081e0bd838028c0965338699bf0c332a8f8c50e
EdwinDev99 <edwindev99@gmail.com>
2025-04-08 16:55:57 +0100

    alert component exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,15 @@
-import Card, { CardBody } from "./assets/components/card";
-import List from "./assets/components/List";
-import Button from "./assets/components/Button";
 import { useState } from "react";
-
-import { AiOutlineLike } from "react-icons/ai";
-
-/**
- * thurty =
- * falsy = 0, "",  false.undefined ,null
- */
+import Alert from "./assets/components/Alert";
 
 function App() {
-  // const list = ["goku", "tanjiro", "gohan", "picoro"];
-  // const handleSelect = (elemento: string) => {
-  //   console.log("imprimiendo", elemento);
-  // };
-
-  // const [isLoading, setIsLoading] = useState(false);
-  // const handleclick = () => {
-  //   setIsLoading(!isLoading);
-  // };
-
-  // return (
-  //   <>
-  //     <Card>
-  //       <CardBody title="Hola mundo" parrafo="Este es el texto" />
-  //       {list.length ? (
-  //         <List data={list} onSelect={handleSelect} />
-  //       ) : (
-  //         "no hay texto"
-  //       )}
-  //     </Card>
-  //     <Button isLoadin={isLoading} onClick={handleclick}>
-  //       Hola mundo
-  //     </Button>
-  //   </>
-  // );
-
-  const [data, setData] = useState(["goku", "tanjiro", "gohan", "picoro"]);
-
-  const addMinion = () => {
-    setData([...data, "minion"]);
+  const [status, setStatus] = useState(true);
+  const toggleStatus = () => {
+    setStatus(!status);
   };
-
-  const delMinion = () => {
-    setData(data.slice(0, -1));
-  };
-
   return (
-    <Card>
-      <Button onClick={addMinion}>
-        agregar
-        <AiOutlineLike color="blue" size={40} />
-      </Button>
-      <Button onClick={delMinion}>
-        aliminar <AiOutlineLike color="blue" size={40} />
-      </Button>
-      <List data={data}></List>
-    </Card>
+    <Alert onClick={toggleStatus} status={status}>
+      Alert
+    </Alert>
   );
 }
-
 export default App;
