0c8697e3b95b6481edd2eb7efc218d28e42ad38a
EdwinDev99 <edwindev99@gmail.com>
2025-04-22 15:50:43 +0100

    effects, loading status, error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import Form from "./assets/components/Form";
+import { use, useEffect, useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+};
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/xusers";
+    setLoading(true);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error(`${response.status}`);
+        return response.json() as Promise<User[]>;
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error: Error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <p>Cargango....</p>;
+  }
+
+  if (error && !loading) {
+    return <p>Ha ocurrido un error {error}</p>;
+  }
+
   return (
-    <div className="container">
-      <Form />
-    </div>
+    <ul>
+      {users.map((u) => (
+        <li key={u.id}>{u.name}</li>
+      ))}
+    </ul>
   );
 }
 export default App;
