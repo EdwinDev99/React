a2f590f77558b8a205f72d4b3b55d3802ed6097a
EdwinDev99 <edwindev99@gmail.com>
2025-04-22 16:26:41 +0100

    reuse code with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import { use, useEffect, useState } from "react";
-
-type User = {
-  id: string;
-  name: string;
-};
+import useUser from "./hooks/useUser";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string>();
-
-  useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/xusers";
-    setLoading(true);
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) throw new Error(`${response.status}`);
-        return response.json() as Promise<User[]>;
-      })
-      .then((data) => {
-        setUsers(data);
-      })
-      .catch((error: Error) => setError(error.message))
-      .finally(() => setLoading(false));
-  }, []);
+  const { users, loading, error } = useUser();
 
   if (loading) {
     return <p>Cargango....</p>;
